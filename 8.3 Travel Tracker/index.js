4e9f69589d3411824173f6719a0befc3d3250591
timfiloye <timfiloye69@gmail.com>
2024-06-30 20:23:21 -0700

    My Bootcamp with Angela Yu

---
+++ 8.3 Travel Tracker/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'world',
+  password: 'Iam4christ*',
+  port: 5432,
+});
+
+db.connect();
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  //Write your code here.
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code)
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
