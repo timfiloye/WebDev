5abd31ba87b1026642033256badc15470a8319e2
timfiloye <timfiloye69@gmail.com>
2024-07-14 21:45:23 -0700

    Updated the env

--- 8.5 Family Travel Tracker/solution.js
+++ 8.5 Family Travel Tracker/solution.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "123456",
+  password: process.env.PASSWORD,
   port: 5432,
 });
 db.connect();
